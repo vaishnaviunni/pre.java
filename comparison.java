0486be515429f62f7630543fda2d790bb0141e00
vaishnaviunni <vaishnavivaishnavi1647@gmail.com>
2025-03-13 11:49:32 +0530

    vaishnnvi

---
+++ comparison.java
@@ -0,0 +1,10 @@
+public class comparison {
+   public static void main(String[]args) {
+      String A="vaishnavi";
+      String B="abiram";
+      Sytstem.out.println(A==B);
+   }
+}
+
+
+
---
+++ substraction.java
@@ -0,0 +1,10 @@
+public class substraction{
+ public static void main(String[]args){
+    int a=50;
+    int b=30;
+    System.out.println(a-b);
+ }
+}
+
+
+    
